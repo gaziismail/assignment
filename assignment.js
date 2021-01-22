d8c6090664bee8680fcd17aac67c7d5575b42712
Ismail Gazi <ismailgazi622@gmail.com>
2021-01-22 23:28:15 +0530

    complt

--- assignment.js
+++ assignment.js
@@ -1,1 +1,46 @@
-// kilometre to metre
+// kilometer to meter
+function kilometerToMeter(kilometer) {
+    var meter = kilometer * 1000;
+    return meter;
+}
+
+
+//budgetCalculator
+function budgetCalculator(watch, phone, laptop) {
+    var watchPrice = watch * 50;
+    var phonePrice = phone * 100;
+    var laptopPrice = laptop * 500;
+
+    var totalPrice = watchPrice + phonePrice + laptopPrice;
+
+    return totalPrice;
+}
+
+
+//hotelCost
+function hotelCost(days) {
+
+    if (days <= 10) {
+
+        var totalCost = 100 * days;
+
+    }
+    else if (days <= 20) {
+
+        var firstitem = 10 * 100;
+        var remaing = days - 10;
+        var seconditem = remaing * 80;
+        var totalCost = firstitem + seconditem;
+
+    }
+    else {
+
+        var firstitem = 10 * 100;
+        var seconditem = 10 * 80;
+        var remaing = days - 20;
+        var thirditem = remaing * 50;
+
+        var totalCost = firstitem + seconditem + thirditem;
+
+    }
+    return totalCost;
